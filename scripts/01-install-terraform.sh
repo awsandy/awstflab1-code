3e58ae42885b7ac67432b98cc17745d222b5e599
awsandy <awsandy@amazon.com>
2020-02-24 12:17:28 +0000

    12.21 terraform

--- scripts/01-install-terraform.sh
+++ scripts/01-install-terraform.sh
@@ -1,4 +1,4 @@
-wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.18_linux_amd64.zip
-unzip terraform_0.12.20_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.21_linux_amd64.zip
+unzip terraform_0.12.21_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-rm -f terraform_0.12.20_linux_amd64.zip
+rm -f terraform_0.12.21_linux_amd64.zip
