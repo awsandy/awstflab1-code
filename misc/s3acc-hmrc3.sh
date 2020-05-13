a2020edb21a6e8f4e4e2f9611f9b22ae33d0a097
awsandy <awsandy@amazon.co.uk>
2020-05-13 15:57:38 +0100

    ho

--- misc/s3acc-hmrc3.sh
+++ misc/s3acc-hmrc3.sh
@@ -5,4 +5,4 @@ aws s3api list-objects-v2 --bucket arn:aws:s3:eu-west-2:566972129213:accesspoint
 # listing - allowed from internet
 aws s3api list-objects --bucket arn:aws:s3:eu-west-2:566972129213:accesspoint/athmrc3docs3
 # denyed as get and put's are not allowed unless vpc-e access point
-aws s3api get-object --key docs3/Doc3.pdf --bucket arn:aws:s3:eu-west-2:566972129213:accesspoint/athmrc3docs3 Doc3a.pdf
+aws s3api get-object --key docs2/Doc2.pdf --bucket arn:aws:s3:eu-west-2:566972129213:accesspoint/athmrc3docs2 Doc2a.pdf
